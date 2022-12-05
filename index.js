61457a3e3ae63c05c3ae47f8802d579c50a9f22e
FrancoMal <franmalfe@gmail.com>
2022-12-04 23:00:37 -0300

    thrid commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+        let resultado;
+        if (numero % 2 === 0) {
+            resultado = numero.toString(2);
+        } else {
+            resultado = numero.toString(16);
+        }
+        return resultado;
     }
