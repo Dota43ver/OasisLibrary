65e902d67218ba5cfb0361dc6d0de43e73549c7c
Facundo aguero <facundoagueroc@gmail.com>
2022-12-04 22:42:00 -0300

    funcion ready

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero%2){
+
+        var dec = numero.toString(2)
+        return dec
+    } else{
+        let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
+    }
     }
