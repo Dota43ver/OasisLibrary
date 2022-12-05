f282aad32c6bb529ce75d2c23cebb89b709a19e6
SantiCuevas <santicuevas34@gmail.com>
2022-12-05 16:06:43 -0300

    caja de pandora

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero%2 === 0) {
+        let binario = [];
+        do {
+            binario.unshift(numero%2);
+            numero = Math.floor(numero/2);
+        } while (numero > 0)
+        return binario.join("");
+    } else {
+        return numero.toString(16).toUpperCase();
+    }
     }
