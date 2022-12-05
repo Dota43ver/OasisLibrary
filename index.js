bc0b9aaed1fd8d3c754632102b892a1ead2a337b
milidziuban <milagrosdziuban@hotmail.com>
2022-12-04 23:28:29 -0300

    mili

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function laCajaDePandora(numero){
-    
+function juani(){
+    return {
+        nombre: "Milagros Dziuban",
+        edad: 23,
+        nacionalidad: "Santa Fe"
     }
+}
+
+
+// como parámetro un número entero. Si es un número par, debe convertirlo a binario y retornarlo, y si es impar, convertirlo a hexadecimal y retornarlo.
+
+
