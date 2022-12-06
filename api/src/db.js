b464c46bd2053e411da5c9dc4cc9479ebd5476fb
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-12-06 19:17:55 -0300

    -

--- api/src/db.js
+++ api/src/db.js
@@ -41,7 +41,7 @@ const { Book, Author, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-
+//.
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
