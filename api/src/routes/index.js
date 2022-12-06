3901848317a01baa318589a4e43c0ef2748b4d7d
Gaston Nicolas Abaca <gaston.abaca11@gmail.com>
2022-12-06 18:19:45 -0300

    Gaston

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
+require("dotenv").config()
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const genresRouter = require('./genresRouter')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/books", booksRouter);
 
+router.use("/books", genresRouter)
 module.exports = router;
