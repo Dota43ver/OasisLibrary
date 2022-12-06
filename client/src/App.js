0c88d58d351692d7e5a9dad243912ee816629c29
Facundo aguero <facundoagueroc@gmail.com>
2022-12-06 19:30:38 -0300

    feat(client/NavBar): create NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {  BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from "./components/Landing/Landing"
+import NavBar from './components/NavBar/NavBar';
 // import Home from "./components/Home/Home"
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <div>
           <Switch>
             <Route exact path='/' component={Landing} />
+            <Route exact path='/a' component={NavBar}/>
             {/* <Route path= "/home" component={Home} /> */}
             </Switch>
         </div>
