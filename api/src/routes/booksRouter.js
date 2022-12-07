89b96ecd9d3a3d02d5e5d81fa43eaa12534adcd5
Santiago Cuevas <santicuevas34@gmail.com>
2022-12-07 17:18:29 -0300

    Post Books: ruta y controlador

--- api/src/routes/booksRouter.js
+++ api/src/routes/booksRouter.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
-const {Genre,Book} = require ('../db');
-const { getBooks } = require('../controllers/getBooks')
+const {Genre, Book} = require ('../db');
+const { getBooks } = require('../controllers/getBooks');
+const { postBooks } = require('../controllers/postBooks');
 
 const booksRouter = Router();
 
@@ -12,5 +13,14 @@ booksRouter.get('/', async (req,res) => {
         res.status(400).send(error.message);
     }
 });
+
+booksRouter.post("/",async (req,res) => { //MA O MENO, falta que traiga bien el genre de la BBDD por ahora trae lo que le mandes y lo crea en el modelo Genre
+    try {
+        await postBooks(req.body);
+        res.status(200).send('Libro creado con éxito');
+    } catch (error) {
+        res.status(400).send(error.message);        
+    }
+})
 
 module.exports = booksRouter;
