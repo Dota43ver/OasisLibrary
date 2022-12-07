bd9024352920636b777d8a201d278c239243c24c
Matias Agustin Ghigi <matiasghigi@gmail.com>
2022-12-07 17:15:42 -0300

    authorRouter: falta la busqueda de autor por query

--- api/src/routes/authorRouter.js
+++ api/src/routes/authorRouter.js
@@ -3,7 +3,6 @@ const {Router} = require('express');
 const {Genre,Book,Author} = require ('../db');
 
 const jsonData = require('../api.json');
-const { all } = require('./genresRouter');
 // console.log(jsonData);
 
 const router = Router()
@@ -11,24 +10,54 @@ const router = Router()
 router.get('/', async (req, res) => {
     
     try{
-        const author = jsonData.map(g => {
-            return(
-                g.author
-            )
-        })
 
-        const filter = [... new Set (author)];
+        const {name} = req.query;
 
-        filter.forEach( g => {
-            Author.findOrCreate({
-                where: {
-                    name: g
-                }
+        if(name) {
+
+            let authorName = jsonData.filter(book => book.author.toLowerCase().includes(name.toLowerCase()))
+
+            if(authorName.length) {
+
+                const filterByName = [... new Set (authorName)]
+
+                filterByName.forEach( a => {
+                    Author.findOrCreate({
+                        where: {
+                            name: a.author
+                        }
+                    })
+                })
+                const authorByName = await Author.findAll();
+                res.status(200).send(authorByName)
+            }
+            else{
+                res.status(404).send({error: 'sorry, we could not find the author'})
+            }
+        }
+        
+        else {
+
+            const author = jsonData.map(g => {
+              return(
+                  g.author
+              )
             })
-        })
 
-        const allAuthors = await Author.findAll();
-        res.status(200).send(allAuthors)
+            const filter = [... new Set (author)];
+
+            filter.forEach( a => {
+                Author.findOrCreate({
+                    where: {
+                        name: a
+                    }
+                })
+            })
+
+            const allAuthors = await Author.findAll();
+            res.status(200).send(allAuthors)
+          
+        }
         
     }
     catch (error) {
