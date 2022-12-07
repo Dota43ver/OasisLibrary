9d8e468a8b8bcb6613c44dd01a98215cf77e2996
Gaston Nicolas Abaca <gaston.abaca11@gmail.com>
2022-12-07 12:43:56 -0300

    Intento de ruta genero

--- api/src/routes/genresRouter.js
+++ api/src/routes/genresRouter.js
@@ -2,9 +2,19 @@ require('dotenv').config();
 const {Router} = require('express');
 const {Genre,Book} = require ('../db');
 
-/* const jsonData = require('../api.json')
-console.log(jsonData); */
+const jsonData = require('../api.json');
+const { json } = require('body-parser');
+
 const router = Router()
+
+const genero = jsonData.map(g => {
+    return(
+     g.genre
+    )
+})
+genero.flat()
+console.log(jsonData)
+
 
 router.get('/',async (req, res) => {
     
@@ -12,18 +22,29 @@ router.get('/',async (req, res) => {
         // Si tengo en la base de datos consumo desde ahi.
         const genresDb = await Genre.findAll();
         if (genresDb.length) return res.json(genresDb)
+        
+        const genres = jsonData.forEach(async g => {
+            await Genre.findOrCreate({
+                where: {
+                    name: g.genre[0]? g.genre[0] : "hola"
+                    
+                }
+            })
+        })
+        
+        console.log(genres);
+        
 
         
-        const genres = jsonData.map(genre => {
-            return{
-                genre: genre.game  
-            }
-        });
+        
+
+    
+             
         res.json(genres)
         // Si no los busco al json
         
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 })
 
