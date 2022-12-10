b88f5f1cbefa8403edb9f3a0ce3a41e792945085
FrancoMal <franmalfe@gmail.com>
2022-12-10 16:36:20 -0300

    filter hecho

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import { CLEAN_CACHE, GET_BOOKS, GET_BOOK_DETAILS, LOCAL_HOST } from "./types";
+import {
+  ALPHABETICAL_SORT,
+  CLEAN_CACHE,
+  GET_BOOKS,
+  GET_BOOK_DETAILS,
+  LOCAL_HOST,
+  PRICE_SORT,
+  SCORE_SORT,
+} from "./types";
 
 export const getBooks = () => (dispatch) => {
   return axios
@@ -22,10 +30,7 @@ export function cleanCache() {
 export function getBookDetails(id) {
   return async function (dispatch) {
     try {
-      var response = await axios.get(
-        `https://6390e9b265ff418311227edc.mockapi.io/api/oasis/books/${id}`
-        // `${LOCAL_HOST}/books/${id}` no conecta con el endpoint
-      );
+      var response = await axios.get(`${LOCAL_HOST}/books/${id}`);
       return dispatch({ type: GET_BOOK_DETAILS, payload: response.data });
     } catch (error) {
       console.log(error);
@@ -34,10 +39,10 @@ export function getBookDetails(id) {
 }
 
 export function postBook(info) {
-  return async function (dispatch){
-    var json = await axios.post("http://localhost:3001/books",info)
-    return json
-  }
+  return async function (dispatch) {
+    var json = await axios.post(`${LOCAL_HOST}/books`, info);
+    return json;
+  };
 }
 
 /* export function getGenres(){
@@ -63,3 +68,23 @@ export const getGenres = () => (dispatch) => {
       console.log(error);
     });
 };
+export function aplhabeticalSort(payload) {
+  return {
+    type: ALPHABETICAL_SORT,
+    payload,
+  };
+}
+
+export function scoreSort(payload) {
+  return {
+    type: SCORE_SORT,
+    payload,
+  };
+}
+
+export function priceSort(payload) {
+  return {
+    type: PRICE_SORT,
+    payload,
+  };
+}
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getBooks } from "../../actions";
+import {
+  aplhabeticalSort,
+  getBooks,
+  priceSort,
+  scoreSort,
+} from "../../actions";
 import Card from "../Card/Card";
 import NavBar from "../NavBar/NavBar";
 import "./Home.css";
@@ -9,29 +14,102 @@ export default function Home() {
   const allBooks = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
+  const [order, setOrder] = useState("");
+
   useEffect(() => {
     dispatch(getBooks());
   }, [dispatch]);
 
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getBooks());
+    // setPage(1);
+  }
+  function handleAlphabeticalSort(e) {
+    e.preventDefault();
+    dispatch(aplhabeticalSort(e.target.value));
+    // setPage(1);
+    setOrder(`Order ${e.target.value}`);
+  }
+  function handlePriceSort(e) {
+    e.preventDefault();
+    dispatch(priceSort(e.target.value));
+    // setPage(1);
+    setOrder(`Order ${e.target.value}`);
+  }
+  function handleScoreSort(e) {
+    e.preventDefault();
+    dispatch(scoreSort(e.target.value));
+    // setPage(1);
+    setOrder(`Order ${e.target.value}`);
+  }
   return (
     <div>
       <NavBar />
-      <h3 className="home">
-        {allBooks && Array.isArray(allBooks) && allBooks.length !== 0
-          ? allBooks.map((el) => {
-              return (
-                <div className="cards">
-                  <Card
-                    Nombre={el.name}
-                    Precio={el.price}
-                    Puntuación={el.score}
-                    Imagen={el.image}
-                  />
-                </div>
-              );
-            })
-          : null}
-      </h3>
+      <div className="filtersDiv">
+        <div>
+          <button className="refreshButton" onClick={handleClick}>
+            Refresh recipes
+          </button>
+        </div>
+        <div className="alphOrder">
+          <select
+            className="select"
+            name="alphabetical"
+            onChange={(e) => handleAlphabeticalSort(e)}
+          >
+            <option disabled selected>
+              Alphabetical
+            </option>
+            <option value="atoz">A to Z</option>
+            <option value="ztoa">Z to A</option>
+          </select>
+        </div>
+        <div className="filterPrice">
+          <select
+            className="select"
+            name="price"
+            onChange={(e) => handlePriceSort(e)}
+          >
+            <option disabled selected>
+              Price
+            </option>
+            <option value="asc">Ascendiente</option>
+            <option value="desc">Descendiente</option>
+          </select>
+        </div>
+        <div className="filterScore">
+          <select
+            className="select"
+            name="score"
+            onChange={(e) => handleScoreSort(e)}
+          >
+            <option disabled selected>
+              Score
+            </option>
+            <option value="asc">Ascendiente</option>
+            <option value="desc">Descendiente</option>
+          </select>
+        </div>
+      </div>
+      <div className="allBooksDiv">
+        <h3 className="home">
+          {allBooks && Array.isArray(allBooks) && allBooks.length !== 0
+            ? allBooks.map((el) => {
+                return (
+                  <div className="cards">
+                    <Card
+                      Nombre={el.name}
+                      Precio={el.price}
+                      Puntuación={el.score}
+                      Imagen={el.image}
+                    />
+                  </div>
+                );
+              })
+            : null}
+        </h3>
+      </div>
     </div>
   );
 }
