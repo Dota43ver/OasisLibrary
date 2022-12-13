118348777f29ebfec2d23bf4ca9b30e28613831b
FrancoMal <franmalfe@gmail.com>
2022-12-13 15:23:21 -0300

    fix img 2

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -15,35 +15,45 @@ export default function Cart() {
   return (
     <div>
       <NavBar />
-      <h1 className="shopTitle">Shop Cart</h1>
-      <div className="totalPrice">
-        <h2 className="cant">
-          Tienes {totalBooks} libros por un total de ${totalPrice}
-        </h2>
-        <div className="checkDiv">
-          <button className="checkoutBtn">Checkout</button>
-        </div>
-      </div>
-      <div className="infoBookCart">
-        <h2 className="detailTitle">Detalles de compra</h2>
-        {cart.map((item) => (
-          <div className="itemCart">
-            <div className="imgDiv">
-              <img className="itemImg" src={item.image} />
-            </div>
-
-            <div className="namePriceDiv">
-              <h3 className="itemName">{item.name}</h3>
-              <h3 className="itemPrice">Precio: ${item.price}</h3>
-            </div>
-            <div className="quantityTotalDiv">
-              <h3 className="itemQuantity">Cantidad: {item.quantity}</h3>
-              <h1 className="itemTotal">Total ${item.price * item.quantity}</h1>
-              <button className="btnDelete">Eliminar</button>
+      {cart.length ? (
+        <div>
+          <h1 className="shopTitle">Shop Cart</h1>
+          <div className="totalPrice">
+            <h2 className="cant">
+              Tienes {totalBooks} libros por un total de ${totalPrice}
+            </h2>
+            <div className="checkDiv">
+              <button className="checkoutBtn">Checkout</button>
             </div>
           </div>
-        ))}
-      </div>
+          <div className="infoBookCart">
+            <h2 className="detailTitle">Detalles de compra</h2>
+            {cart.map((item) => (
+              <div className="itemCart">
+                <div className="imgDiv">
+                  <img className="itemImg" src={item.image} />
+                </div>
+
+                <div className="namePriceDiv">
+                  <h3 className="itemName">{item.name}</h3>
+                  <h3 className="itemPrice">Precio: ${item.price}</h3>
+                </div>
+                <div className="quantityTotalDiv">
+                  <h3 className="itemQuantity">Cantidad: {item.quantity}</h3>
+                  <h1 className="itemTotal">
+                    Total ${item.price * item.quantity}
+                  </h1>
+                  <button className="btnDelete">Eliminar</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="totalPrice">
+          <h2 className="cant">No hay libros en el carrito</h2>
+        </div>
+      )}
     </div>
   );
 }
