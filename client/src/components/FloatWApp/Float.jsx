4f235cd305052cee9c7467c1c93780ce100caf1d
milidziuban <milagrosdziuban@hotmail.com>
2022-12-23 11:02:05 -0300

    chatonline

---
+++ client/src/components/FloatWApp/Float.jsx
@@ -0,0 +1,27 @@
+export default function Float() {
+
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/63a5ae9ec2f1ac1e2029d0ad/1gkvj47lh';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+
+    return (
+
+        <div>
+
+
+            <script type="text/javascript">
+
+            </script>
+
+
+
+        </div>
+
+    )
+}
