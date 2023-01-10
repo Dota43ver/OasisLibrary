3fab00683c408085e952a2edd66c6f24e5274e16
milidziuban <milagrosdziuban@hotmail.com>
2023-01-10 12:46:36 -0300

    navdash

--- client/src/components/Dashboard Admin/Dashboard.jsx
+++ client/src/components/Dashboard Admin/Dashboard.jsx
@@ -1,39 +1,24 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { getUsers } from "../../actions";
+import NavBarDash from "../NavBarDash/NavBarDash";
 import style from "./Dashboard.module.css";
-import oasis from "./oasis.jpg";
 
 export default function Dashboard() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+  
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
+
+
   return (
     <div>
+      <NavBarDash></NavBarDash>
       {" "}
       {user && user.role === "admin" ? (
-        <div className={style.barra}>
-          <div className={style.barraContent}>
-            <Link to="/home">
-              <img src={oasis} alt="" width="114px" height="105px" />
-            </Link>
-            <h2 className={style.title}>Dashboard Admin</h2>
-          </div>
-          <div className={style.linkCreate}>
-            <Link to="/dashboard/bookcreate">
-              <button className={style.createBtn}>Add Books</button>
-            </Link>
-            <Link to="/dashboard/bookedit">
-              <button className={style.createBtn}>Edit book</button>
-            </Link>
-            <Link to="/dashboard/users">
-              <button className={style.createBtn}>Users</button>
-            </Link>
-          </div>
-        </div>
+        <div> admin </div>
       ) : (
         <div className={style.content}>
           <h3>Registered Users Only</h3>
---
+++ client/src/components/NavBarDash/NavBarDash.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getUsers } from "../../actions";
+import oasis from "./oasis.jpg";
+import './NavBarDash.css'
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+export default function NavBarDash() {
+    const dispatch = useDispatch();
+
+    const user = useSelector((state) => state.user);
+    useEffect(() => {
+        dispatch(getUsers());
+    }, [dispatch]);
+
+    return (
+        <div className='main_container'>
+
+                <Link to="/home">
+                    <div className='container_image'>
+                        <img src={oasis} alt="" width="110px" height="100px" />
+                    </div>
+                </Link>
+
+
+                <h2 className='titleDash'>Dashboard Admin</h2>
+
+
+                <div className='linkCreate'>
+                    <Link to="/dashboard/bookcreate">
+                        <button className='createBtn'> <AddIcon/> Add Books</button>
+                    </Link>
+                    <Link to="/dashboard/bookedit">
+                        <button className='createBtn'> <EditIcon/> Edit book</button>
+                    </Link>
+                    <Link to="/dashboard/users">
+                        <button className='createBtn'> <PersonOutlineIcon/> Users</button>
+                    </Link>
+                </div>
+        </div>
+    );
+}
