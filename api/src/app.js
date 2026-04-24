87cd213058c43cdf03ee273cbdfcbcd49272b0ce
facundo aguero <facundoagueroc@gmail.com>
2026-04-24 00:58:53 -0300

    problema de cors con vercel

--- api/src/app.js
+++ api/src/app.js
@@ -3,35 +3,32 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require("cors")
+const cors = require("cors");
 
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
-server.use(cors())
+
+const corsOptions = {
+  origin: 'https://oasis-library.vercel.app',
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'token', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+server.use(cors(corsOptions));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(express.json())
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'https://oasis-library.vercel.app'); // update to match the domain you will make the request from
-  // res.header('Access-Control-Allow-Credentials', 'true');
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Credentials", "true");
-res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-// res.setHeader('Access-Control-Allow-Headers', '*')
-res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-   next();
-});
+server.use(express.json());
+
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
